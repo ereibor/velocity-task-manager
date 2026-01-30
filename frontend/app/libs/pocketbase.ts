df0f9675afd236720e2b3dea4c420e8f0b60f2af
ereibor <etunere@gmail.com>
2026-01-30 10:12:04 +0100

    fix pocketbase deployment

--- frontend/app/libs/pocketbase.ts
+++ frontend/app/libs/pocketbase.ts
@@ -1,5 +1,5 @@
 import PocketBase from "pocketbase";
 
 export const pb = new PocketBase(
-  process.env.NEXT_PUBLIC_PB_URL || "http://127.0.0.1:8090",
+  process.env.NEXT_PUBLIC_POCKETBASE_URL || "http://127.0.0.1:8090",
 );
